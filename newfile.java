dde4cf405d96114e51e99b20677351acee3c6018
PavanNavghare <128789376+PavanNavghare@users.noreply.github.com>
2023-03-30 11:16:27 +0530

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args)
+	 {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the table no :");
+    int table=sc.nextInt();
+    
+		for(int i=1 ; i<=10; i++)
+	
+		{	System.out.println(table +"*"+ i + "="+ table*i);
+		}
+			
+	}
+}
